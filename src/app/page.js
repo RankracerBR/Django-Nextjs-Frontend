6ac829d059922a719b1fec4482be3cd7dd16f491
Augusto Pontes <augustopontes010@gmail.com>
2024-10-19 04:37:13 -0300

    Front-End

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+"use client"
+
 import Image from "next/image";
 
 export default function Home() {
+  async function getDjangoAPIData(){
+    const response = await fetch("http://127.0.0.1:8000/api/hello");
+    const data = await response.json();
+    console.log(data)
+  }
+
+  async function handleClick(){
+    await getDjangoAPIData()
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <button onClick={handleClick}>
+          Lookup Data
+        </button>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
